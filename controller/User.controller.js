79015f0e3530e4e0988c87e794ec7113d6108e02
shre1shetty <shrevan506@gmail.com>
2025-12-07 18:22:57 +0530

    same site none for cross origin

--- controller/User.controller.js
+++ controller/User.controller.js
@@ -57,6 +57,7 @@ export const LoginUser = async (req, res) => {
       secure: true,
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      sameSite: "none",
     });
     const bodyToSend =
       process.env.NODE_ENV === "production"
